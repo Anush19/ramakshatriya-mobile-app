bf7bc703d1a595d6fd60a5c3c8723961cc2db4f5
ABA735 <Anushree.banerjee@maersk.com>
2022-12-02 20:44:03 +0530

    edit dynamic message

--- src/app/hometab/upload-form/upload-form.page.ts
+++ src/app/hometab/upload-form/upload-form.page.ts
@@ -91,9 +91,7 @@ export class UploadFormPage{
 
   onWillDismiss(event: Event) {
     const ev = event as CustomEvent<OverlayEventDetail<string>>;
-    if (ev.detail.role === 'confirm') {
-      this.message = `Hello, ${ev.detail.data}!`;
-    }
+    //TODO LATER
   }
   navigateBack() {
     this.router.navigateByUrl('/tabs/hometab/home');
